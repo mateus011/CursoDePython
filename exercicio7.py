96e2be01b5cab419119afb9e84a20585f196cc7e
Mateus <mateus16499@gmail.com>
2025-02-27 18:38:25 -0300

    exercicio da estrutura de repetição for

---
+++ exercicio7.py
@@ -0,0 +1,6 @@
+# criar um retangulo 6x6 de @
+for l in range(6):
+    for j in range(6):
+        print('*', end='') 
+    print()
+
---
+++ exercicio8.py
@@ -0,0 +1,10 @@
+# triangulo com o simbolo @
+n = 8
+simbol = '@'
+for i in range(n):
+    for j in range(0,i+1):
+        print(f'{simbol}',end=' ')
+    print()
+
+
+
