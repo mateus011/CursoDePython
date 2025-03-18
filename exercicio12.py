ec633339c790dda7d7a434ad9687fcfe53b1f313
Mateus <mateus16499@gmail.com>
2025-03-18 11:06:18 -0300

    aprendendo sobre listas e manipulação de listas

---
+++ exercicio12.py
@@ -0,0 +1,19 @@
+# creianção de uma lista 
+def inserir_numlist():
+    numeros =  [1,2,3,4,5,6,7,8,9] #tabela type list
+    num = int(input('coloque um numero na lista: ')) #pegando numero do usuario
+    numeros.append(num) # função para colocar o  numero no final da lista 
+    print(f'{numeros}, é o numero insirido é {num}')# imprimir na tela
+     
+
+def remove_numlist():
+    numeros =  [1,2,3,4,5,6,7,8,9] #tabela type list
+    num = int(input('coloque o numero que deseja apagar: '))
+    numeros.remove(num)
+    print(f'{numeros}, é o numero que foi removido da lista foi o {num}')
+
+def organizar_alfabetica():
+    nomes = ['mateus','jason','clayde','vasconselo','aride','letes']
+    nomes.sort()
+    print(f'{nomes}')
+
---
+++ exercicio13.py
@@ -0,0 +1,9 @@
+# concatenando listas
+
+def jutando_listas():
+    numeros = [1,2,3,4,5,6,7,8,9]
+    letras = ['a','b','c','d','e','f','g','h','i']
+    numeros.extend(letras)
+    print(numeros)
+
+
